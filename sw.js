034500587afd127539f0eb7fa3a46a28d29fabf9
Jan Hořava <czechinthemail@gmail.com>
2026-04-27 06:09:12 -0700

    added files

---
+++ sw.js
@@ -0,0 +1,40 @@
+'use strict';
+
+function handle_activate(event){
+    event.waitUntil(
+      caches.keys()
+        .then(keys => Promise.all(
+          keys.map(key => (key === id
+            ? Promise.resolve()
+            : caches.delete(key))
+          )
+        )).then(() => self.clients.claim())
+    );
+}
+
+function handle_fetch(event){
+    event.respondWith(
+      caches.match(event.request).then(cached => cached
+        || fetch(event.request).catch(() => {
+            if(event.request.mode === 'navigation'){
+                return caches.match('index.htm');
+            }
+        })
+      )
+    );
+}
+
+function handle_install(event){
+    event.waitUntil(
+      caches.open(id)
+        .then(cache => cache.addAll(files))
+        .then(() => self.skipWaiting())
+    );
+}
+
+self.addEventListener('activate', handle_activate);
+self.addEventListener('fetch', handle_fetch);
+self.addEventListener('install', handle_install);
+
+const id = 'rezist_x';
+const files = ['.', 'index.htm', 'manifest.json', '192.png', '512.png'];
